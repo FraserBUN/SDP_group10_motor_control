571f68a7ed377f50cf9a4a767063dea6f4360d4d
Fraser Bunting <Fraser.ianjames@gmail.com>
2023-03-28 10:07:35 +0100

    updating to match what has worked
    This was the code on the arduino when we were running tests on power, signals

--- arduino/servo-control.cpp
+++ arduino/servo-control.cpp
@@ -1,112 +1,63 @@
-#include <servo.h>
+#include <Servo.h>
 
-class ServoWrapper
-{
-    public:
-        void attach(int pin)
-        {
-            servo.attach(pin);
-        }
-
-        ServoWrapper(Servo servo, double pos)
-        {
-            this->servo = servo;
-            this->pos = pos;
-        }
-
-
-        void lift()
-        {
-            servo.write(pos);
-            delay(750);
-        }
-
-        void lower()
-        {
-            servo.write(0);
-            delay(750);
-        }
-
-        void actuate()
-        {
-            lift();
-            lower();
-        }
-
-    private:
-        Servo servo;
-        double pos;
-};
-
-Servo southKernel;
-Servo eastKernel;
-Servo westKernel;
-Servo northKernel;
-Servo centreKernel;
-Servo extraSouthKernel;
-
-ServoWrapper south(southKernel, 110);
-ServoWrapper east(eastKernel, 110);
-ServoWrapper west(westKernel, 110);
-ServoWrapper north(northKernel, 150);
-ServoWrapper centre(centreKernel, 110);
-ServoWrapper extraSouth(extraSouthKernel, 110);
-
+Servo south;
+Servo east;
+Servo west;
+Servo north;
+Servo centre;
+Servo extraSouth;
 
 void setup()
 {
     Serial.begin(9600);//Set Baud Rate to 9600 bps
-    south.attach(9);
-    east.attach(10);
-    west.attach(11);
-    north.attach(12);
-    centre.attach(13);
-    extraSouth.attach(8);
+    south.attach(3);
+    east.attach(5);
+    west.attach(6);
+    north.attach(9);
+    centre.attach(10);
+
 }
 
-void actuate(Servo servo)
+void actuate(Servo servo, double pos)
 {
-    servo.write(pos);
-    delay(750);
     servo.write(0);
+    delay(750);
+    servo.write(pos);
 }
 
 void shirt_short()
 {
-    extraSouth.actuate();
-    south.actuate();
-    east.actuate();
-    west.actuate();
-    centre.actuate();
-    north.actuate();
+    actuate(south, 100);
+    actuate(east, 100);
+    actuate(west, 100);
+    actuate(centre, 100);
+    actuate(north, 150);
 }
 
 void shirt_long()
 {
-    extraSouth.actuate();
-    south.actuate();
-    east.actuate();
-    west.actuate();
-    east.actuate();
-    west.actuate();
-    centre.actuate();
-    north.actuate();
+    actuate(south, 100);
+    actuate(east, 100);
+    actuate(west, 100);
+    actuate(east, 100);
+    actuate(west, 100);
+    actuate(centre, 100);
+    actuate(north, 150);
 }
 
 void trousers()
 {
-    south.actuate();
-    east.actuate();
-    west.actuate();
-    centre.actuate();
-    north.actuate();
+    actuate(centre, 100);
+    actuate(east, 100);
+    actuate(west, 100);
+    actuate(north, 150);
 }
 
 void loop()
 {
     if (Serial.available() > 0)
     {
-        string usb = Serial.read();
+        char usb = Serial.read();
         if (usb == '1')
         {
             shirt_short();
@@ -115,7 +66,8 @@ void loop()
         {
             shirt_long();
         }
-        else if (usb == '3') {
+        else if (usb == '3')
+        {
             trousers();
         }
     }
