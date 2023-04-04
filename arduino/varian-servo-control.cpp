eb90e6fa8b158c38dbfd25d039c1df099dd7018a
Fraser Bunting <Fraser.ianjames@gmail.com>
2023-04-04 12:51:56 +0100

    sending code to varian

---
+++ arduino/varian-servo-control.cpp
@@ -0,0 +1,181 @@
+void setup()
+{
+    Serial.begin(9600);//Set Baud Rate to 9600 bps
+    south.attach(3);
+    east.attach(5);
+    west.attach(6);
+    north.attach(10);
+    centre.attach(9);
+
+}
+
+void up(Servo servo)
+{
+  servo.write(0);
+}
+
+void down (Servo servo)
+{
+  servo.write(75);  
+}
+
+void actuate(Servo servo, double pos)
+{
+    servo.write(0);
+    delay(750);
+    servo.write(pos);
+}
+
+void shirt_short()
+{
+    actuate(south, 100);
+    actuate(east, 100);
+    actuate(west, 100);
+    actuate(centre, 100);
+    actuate(north, 150);
+}
+
+void shirt_long()
+{
+    actuate(south, 100);
+    actuate(east, 100);
+    actuate(west, 100);
+    actuate(east, 100);
+    actuate(west, 100);
+    actuate(centre, 100);
+    actuate(north, 150);
+}
+
+void trousers()
+{
+    actuate(centre, 100);
+    actuate(east, 100);
+    actuate(west, 100);
+    actuate(north, 150);
+}
+
+void loop() {
+  if (Serial.available() > 0) {
+    char usb = Serial.read();
+        if (usb == '1')
+        {
+            shirt_short()
+            // actuate(south, 100);
+        }
+        if (usb == '2')
+        {
+            shirt_long()
+            // actuate(east, 100);
+#include <Servo.h>
+
+Servo south;
+Servo east;
+Servo west;
+Servo north;
+Servo centre;
+int up_time = 400;
+int down_time = 500;
+
+void setup()
+{
+    Serial.begin(9600);//Set Baud Rate to 9600 bps
+    south.attach(3);
+    east.attach(5);
+    west.attach(6);
+    north.attach(10);
+    centre.attach(9);
+
+}
+
+
+void up(Servo servo)
+{
+  servo.write(0);
+}
+
+void down (Servo servo)
+{
+  servo.write(75);  
+}
+
+void north_up(){
+    north.write(80);
+  }
+
+void north_down(){
+    north.write(0);
+  }
+
+void loop()
+{
+  down(south);
+  down(centre);
+  down(west);
+  down(east);
+      if (Serial.available() > 0)
+    {
+        char usb = Serial.read();
+        if (usb == '1')
+        {
+            up(south)
+            delay(up_time)
+  
+        }
+        if (usb == '2')
+        {
+            up(east)
+            delay(up_time)
+  
+        }
+        if (usb == '3')
+        {
+            up(west);
+            delay(up_time)
+  
+        }
+        if (usb == '4')
+        {
+            north_up();
+            delay(500);
+  
+        }
+        if (usb == '5')
+        {
+            down(centre);
+            delay(up_time);
+  
+        }
+        if (usb == '6')
+        {
+            down(south)
+            delay(down_time)
+  
+        }
+        if (usb == '7')
+        {
+            down(east)
+            delay(down_time)
+  
+        }
+        if (usb == '8')
+        {
+            down(west);
+            delay(down_time)
+  
+        }
+        if (usb == '9')
+        {
+            north_down();
+            delay(750);
+  
+        }
+        if (usb == 'a')
+        {
+            down(centre);
+            delay(down_time);
+  
+        }
+    }
+}
+
+}
